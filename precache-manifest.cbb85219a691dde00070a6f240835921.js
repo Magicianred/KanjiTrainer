8256058a27e5ca59c68cca2029b2c39b03b3d64a
roro <roro@localhost.localdomain>
2020-05-05 20:29:09 +0100

    Updates

---
+++ precache-manifest.cbb85219a691dde00070a6f240835921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19cfcbb30138780f8d8ee9215a6c7862",
+    "url": "/KanjiTrainer/index.html"
+  },
+  {
+    "revision": "a37b308efcd23f6eca9d",
+    "url": "/KanjiTrainer/static/css/main.060891f1.chunk.css"
+  },
+  {
+    "revision": "392e2fcd1ca0a2aa3f1a",
+    "url": "/KanjiTrainer/static/js/2.33a546c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/KanjiTrainer/static/js/2.33a546c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a37b308efcd23f6eca9d",
+    "url": "/KanjiTrainer/static/js/main.3e4b8323.chunk.js"
+  },
+  {
+    "revision": "b2872dbb214a8d83d588",
+    "url": "/KanjiTrainer/static/js/runtime-main.6eb0301f.js"
+  }
+]);

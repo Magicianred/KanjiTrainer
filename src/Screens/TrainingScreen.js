3bcc3a6f2e464e92afdf7e62a3dcfb169334f6a1
roro <roro@localhost.localdomain>
2020-05-05 17:24:23 +0100

    show button

--- src/Screens/TrainingScreen.js
+++ src/Screens/TrainingScreen.js
@@ -5,31 +5,43 @@ export default function TrainingScreen() {
   const randomNumberGenerator = (n) => {
     return Number(Math.floor(Math.random() * n));
   };
-
   const [randomNum, setRandomNum] = useState(randomNumberGenerator(850));
+  const [answer, setAnswer] = useState("");
+  const KanjiNt = data[0].Kanji.DATA[randomNum];
+  const ans = [
+    KanjiNt.kunyomi + " / " + KanjiNt.onyomi + " / " + KanjiNt.meaning,
+  ];
   const showKanji = () => {
-    const KanjiNt = data[0].Kanji.DATA[randomNum];
     console.log(KanjiNt.kanji);
 
     return (
       <div>
         <p key={data.Kanji}>{KanjiNt.kanji}</p>
-        <p key={data.Kanji}>
-          {KanjiNt.kunyomi + " / " + KanjiNt.onyomi + " / " + KanjiNt.meaning}
-        </p>
       </div>
     );
   };
 
-  const handleButton = () => {
+  const handelNext = () => {
     setRandomNum(randomNumberGenerator(850));
+    setAnswer([]);
+  };
+
+  const handleShow = () => {
+    setAnswer(ans);
+    console.log(ans);
   };
 
   return (
     <div>
       {showKanji()}
 
-      <button onClick={(e) => handleButton(e)}> Next </button>
+      <button onClick={(e) => handelNext(e)}> Next </button>
+      <br />
+      <hr />
+      <button onClick={(e) => handleShow(e)}> Show </button>
+      <div>
+        <p>{answer}</p>
+      </div>
     </div>
   );
 }
